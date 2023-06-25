6b59830a37a29885b721683783d120812e152173
DeadlyWhiskers <133860751+DeadlyWhiskers@users.noreply.github.com>
2023-06-26 02:58:40 +0300

    added some functions for admin

--- superproekt.cpp
+++ superproekt.cpp
@@ -7,43 +7,200 @@ using namespace std;
 
 int main()
 {
-    cout<<"Who are you?\nAdmin-1\nSeller-2\nOur Client-3\n";
-    bool correct = false, StillRunning = true;
+    productlist Products;
+    int userid;
+    bool correct = false, StillRunning = true, roleChosen = false;
     char dec;
+    int quant, price;
+    string desc, name;
     user *User;
-    while(!correct)
+    while (StillRunning)
     {
-    char dec = getch();
-    switch(dec)
-    {
-        case('1'): 
-        User = new admin();
-        correct = 1;
-        break;
-        case('2'):
-        User = new seller();
-        correct = 1;
-        break;
-        case('3'):
-        User = new client();
-        correct = 1;
-        break;
-        default:
-        break; 
-    }
-    }
-    while(StillRunning)
-    {
-        system("cls");
-        switch(User->getId())
+        correct = false;
+        while (!correct && !roleChosen)
         {
-            case(1): cout<<"You are cool admin"<<endl; break;
-            case(2): cout<<"Go make money, dog"<<endl; break;
-            case(3): cout<<"Welcome back our dear client!"<<endl; break;
-            default: cout<<"error"<<endl;
+        system("cls");
+        cout << "Who are you?\nAdmin-1\nSeller-2\nOur Client-3\n";
+        char dec = getch();
+        switch (dec)
+        {
+        case ('1'):
+            User = new admin(&Products);
+            correct = 1;
+            roleChosen = 1;
+            break;
+        case ('2'):
+            User = new seller(&Products);
+            correct = 1;
+            roleChosen = 1;
+            break;
+        case ('3'):
+            User = new client(&Products);
+            cout<<"Enter your id - ";
+            cin>>userid;
+            User->setId(userid);
+            correct = 1;
+            roleChosen = 1;
+            break;
+        default:
+            correct = 0;
+            break;
         }
-        StillRunning = false;
+        }
+        system("cls");
+        correct = 0;
+        switch (User->getUserType())
+        {
+        case (1):
+        {
+            cout << "You are cool admin" << endl;
+            cout<< "Add new product - 1\nDelete existing product - 2\nChange product's info - 3\nRestock product - 4\nShow products - 5\nChange role - 6\nClose program - 0"<<endl;
+            while(!correct)
+            {
+                fflush(stdin);
+                dec = getch();
+                switch(dec)
+                {
+                    case('1'):
+                    {
+                        correct = 1;
+                        fflush(stdin);
+                        cout<<"Enter name:";
+                        getline(std::cin, name);
+                        cout<<"Enter price:";
+                        cin>>price;
+                        cout<<"Enter description:";
+                        fflush(stdin);
+                        getline(std::cin, desc);
+                        cout<<"Enter quantity:";
+                        cin>>quant;
+                        User->addProduct(name, price, desc, quant);
+                        break;
+                    }
+                    case('2'):
+                    {
+                        correct = 1;
+                        break;
+                    }
+                    case('3'):
+                    {
+                        correct = 1;
+                        break;
+                    }
+                    case('4'):
+                    {
+                        correct = 1;
+                        break;
+                    }
+                    case('5'):
+                    {
+                        correct = 1;
+                        system("cls");
+                        if(!User->getProductList().empty())
+                        {
+                            for(product i : User->getProductList())
+                            {
+                                cout<<i.getName()<<endl
+                                <<i.getPrice()<<endl
+                                <<i.getDesc()<<endl
+                                <<i.getQuantity()<<endl<<endl;
+                            }
+                        }
+                        getch();
+                        break;
+                    }
+                    case('6'):
+                    {
+                        roleChosen = 0;
+                        correct = 1;
+                        delete User;
+                        break;
+                    }
+                    case('0'):
+                    {
+                        StillRunning = 0;
+                        correct = 1;
+                        break;
+                    }
+                    default: correct = 0; break;
+
+                }
+            }
+            break;
+        }
+        case (2):
+        {
+            cout << "Go make money, dog" << endl;
+            cout<< "Show order list - 1\nApprove order - 2\nApprove payment - 3\nShip order - 4\nChange role - 5\nClose program - 0"<<endl;
+            while(!correct)
+            {
+                fflush(stdin);
+                dec = getch();
+                switch(dec)
+                {
+                    case('1'):
+                    {
+                        correct = 1;
+                        break;
+                    }
+                    case('5'):
+                    {
+                        roleChosen = 0;
+                        correct = 1;
+                        delete User;
+                        break;
+                    }
+                    case('0'):
+                    {
+                        StillRunning = 0;
+                        correct = 1;
+                        break;
+                    }
+                    default: correct = 0; break;
+
+                }
+            }
+            break;
+        }
+        case (3):
+        {
+            cout << "Welcome back our dear client!" << endl;
+            cout<< "Create order - 1\nCancel order - 2\nPay for order - 3\nShow order - 4\nChange role - 5\nClose program - 0"<<endl;
+            while(!correct)
+            {
+                fflush(stdin);
+                dec = getch();
+                switch(dec)
+                {
+                    case('1'):
+                    {
+                        correct = 1;
+                        break;
+                    }
+                    case('5'):
+                    {
+                        roleChosen = 0;
+                        correct = 1;
+                        delete User;
+                        break;
+                    }
+                    case('0'):
+                    {
+                        StillRunning = 0;
+                        correct = 1;
+                        break;
+                    }
+                    default: correct = 0; break;
+
+                }
+            }
+            break;
+        }
+        default:
+        {
+            cout << "error" << endl;
+        }
+        }
     }
-    getch();
     delete User;
 }
--- superproekt.hpp
+++ superproekt.hpp
@@ -8,13 +8,33 @@ class product
     private:
     int price, quantity;
     string name, desc;
+    public:
+    int getPrice(){return price;}
+    int getQuantity(){return quantity;}
+    string getName(){return name;}
+    string getDesc(){return desc;}
+    product(string name, int price, string desc, int quant)
+    {
+        this->name = name;
+        this->price = price;
+        this->desc = desc;
+        this->quantity = quant;
+    }
 };
 
 class order
 {
     product Prod;
     bool paid;
-    int condition;
+    int state, clientid;
+    /*
+    1 - default
+    2 - vaiting for payment approval 
+    3 - vaiting for approval 
+    4 - payment approved 
+    5 - fully approved 
+    6 - shipped
+    */
 };
 
 class orderList
@@ -31,27 +51,64 @@ class productlist
     {
         return productlistArray;
     }
+    void addProduct(string name, int price, string desc, int quant)
+    {
+        productlistArray.push_back(product(name, price, desc, quant));
+    }
 };
 
 class user{
-    vector<product> getProdList(productlist pl)
-    {
-        return pl.getArray();
-    }
+
     public:
-    virtual int getId() = 0;
+    virtual int getUserType() = 0;
+    productlist* currentlist;
+    //client
+    virtual int getId(){};
+    virtual void setId(int id){};
+
+    vector<product> getProductList()
+    {
+        return currentlist->getArray();
+    };
+
+
+    user(){};
+    user(productlist *pl)
+    {
+        currentlist = pl;
+    }
+
+    //admin
+    virtual void addProduct(string name, int price, string desc, int quant){}
 };
 
 class admin: public user{
-    int getId() {return 1;}
     public:
+    int getUserType() {return 1;}
+    virtual void addProduct(string name, int price, string desc, int quant)
+    {
+        currentlist->addProduct(name,price,desc,quant);
+    }
+
+    admin(productlist *pl): user(pl){}
 };
 
 class seller: public user{
     public:
-    int getId() {return 2;}
+    int getUserType() {return 2;}
+
+    seller(productlist *pl): user(pl){}
 };
+
 class client: public user{
+    private:
+    int id;
     public:
-    int getId() {return 3;}
+
+    client(productlist *pl): user(pl){}
+
+    int getUserType() {return 3;}
+    client() {id = 0;}
+    virtual int getId() {return id;}
+    virtual void setId(int id){this->id = id;}
 };
